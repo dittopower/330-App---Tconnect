298a9773a3f06f6fb3f5dca4d7402b497795f2ec
roflmonsterjh <roflmonster.jh@gmail.com>
2015-09-17 02:37:26 +1000

    Actual Project

---
+++ NoteTaker1/NoteTaker1/Views/Feed.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NoteTaker1
+{
+	public partial class Feed : ContentPage
+	{
+		public Feed ()
+		{
+			InitializeComponent ();
+
+			Title = "Home/Feed";
+
+			var noteList = new List<Notes> ();
+
+			noteList.Add (new Notes ("Meeting with Cellcare"){ TimeStamp = "11:00 am"});
+			noteList.Add (new Notes ("Recollection Meeting"){ TimeStamp = "12:00 pm" });
+			noteList.Add (new Notes ("Visit Brisbane branch"){ TimeStamp = "1:00 pm"});
+			noteList.Add (new Notes ("Visit Carindale branch"){ TimeStamp = "2:30 pm"});
+
+			NoteListView.ItemsSource = noteList;
+		}
+	}
+}
+
---
+++ NoteTaker1/NoteTaker1/Views/TempMenu.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NoteTaker1
+{
+	public partial class TempMenu : ContentPage
+	{
+		public TempMenu ()
+		{
+			InitializeComponent ();
+			Title = "Temporary Menu";
+		}
+
+		protected void ButtonCreateEvent(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new CreateEvent ());				
+		}
+
+		protected void ButtonUserAccount(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new UserAccount ());				
+		}
+
+		protected void ButtonCalendar(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new Calendar ());				
+		}
+
+		protected void ButtonFeed(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new Feed ());			
+		}
+
+		protected void ButtonEventView(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new EventView ());			
+		}
+
+		protected void ButtonContacts(object sender, EventArgs e)
+		{
+			Navigation.PushAsync (new Contacts ());			
+		}
+	}
+}
+
