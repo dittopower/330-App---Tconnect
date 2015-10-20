ac947e8b0bd7cd8a9d69cdec09196fd7b900928e
Damon <dittopower@gmail.com>
2015-10-20 15:46:28 +1000

    Create Event v1.1

--- Tconnect/Tconnect.Data/ViewModel/EventCreateViewModel.cs
+++ Tconnect/Tconnect.Data/ViewModel/EventCreateViewModel.cs
@@ -4,13 +4,14 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using Tconnect.Data.ViewModel;
 using Microsoft.Practices.ServiceLocation;
+using System.Diagnostics;
 
 namespace Tconnect.Data
 {
 	public class EventCreateViewModel :ViewModelBase
 	{
 		public ICommand SaveNoteCommand { get; private set;}
-		private String noteTitle;
+		private String noteTitle = "";
 
 		public String NoteTitle
 		{
@@ -20,19 +21,22 @@ namespace Tconnect.Data
 		}
 
 
-		private DateTime noteDate;
+		private DateTime noteDate = DateTime.Now;
 		//make this a date at some point
 		public DateTime NoteDate
 		{
 			get { return noteDate.Date; }
 			set { noteDate = value.Date.Add(noteDate.TimeOfDay);
-				RaisePropertyChanged(() => NoteTitle); }
+				RaisePropertyChanged(() => NoteDate); }
 		}
 		public TimeSpan NoteTime{
 			get{ return noteDate.TimeOfDay; }
-			set{ noteDate = noteDate.Date.Add (value); }
+			set{ noteDate = noteDate.Date.Add (value);
+				RaisePropertyChanged(() => NoteTime);
+				Debug.WriteLine (noteDate);
+			}
 		}
-		private string eventDetails;
+		private string eventDetails = "";
 
 		public string EventDetails
 		{
@@ -41,7 +45,7 @@ namespace Tconnect.Data
 				RaisePropertyChanged(() => EventDetails); }
 		}
 
-		private string noteLocationText;
+		private string noteLocationText = "";
 
 		public string NoteLocationText
 		{
