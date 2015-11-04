4c750644df535c81a1f120fab91dedf56303e9fa
JordanBeak <jordanbeak95@gmail.com>
2015-11-04 23:49:07 +1000

    Created and implemented menu button
    Created and implemented menu button.

--- Tconnect/Droid/Resources/Resource.designer.cs
+++ Tconnect/Droid/Resources/Resource.designer.cs
@@ -58,13 +58,16 @@ namespace Tconnect.Droid
 			public const int IcoHome = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int icon = 2130837507;
+			public const int IcoMenu = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int IcoProf = 2130837508;
+			public const int icon = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int ProfileImage = 2130837509;
+			public const int IcoProf = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int ProfileImage = 2130837510;
 			
 			static Drawable()
 			{
